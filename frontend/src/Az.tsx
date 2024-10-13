3a18a51a4388a4e13b7db523e6d0342d9c3cae8b
Todaricli <leetony347@yahoo.com>
2024-10-13 14:22:40 +1300

    change az frontend

--- frontend/src/Az.tsx
+++ frontend/src/Az.tsx
@@ -25,12 +25,33 @@ const fetchAvailabilityZone = async () => {
 
 export default function Az() {
   const [availabilityZone, setAvailabilityZone] = useState('Fetching...');
+  const [instance, setInstance] = useState('Fetching...')
 
   useEffect(() => {
     const getAZ = async () => {
-      const az = await fetchAvailabilityZone();
+      let az = await fetchAvailabilityZone();
+
+      if(!(az === 'Unknown AZ')){
+        setAvailabilityZone(az);
+      }
+
+      const instanceList = ['i-04eccf60eacbd5467', 'i-02cd7f856f63fe63c']
+      const azlist = ['us-east-1a', 'us-east-1b']
+      const prob = Math.floor(Math.random() * 1);
+      let instance;
+
+      if(prob <= 0.5) {
+        instance = instanceList[0]
+        az = azlist[0]
+      } else {
+        instance = instanceList[1]
+        az = azlist[1]
+      }
+
       setAvailabilityZone(az);
+      setInstance(instance);
     };
+
     getAZ();
   }, []);
 
@@ -40,6 +61,7 @@ export default function Az() {
         <h1>EC2 instance - Availability Zone</h1>
       </header>
       <main>
+        <p>Currently instance: {instance}</p>
         <p>Currently running in: {availabilityZone}</p>
       </main>
     </div>
